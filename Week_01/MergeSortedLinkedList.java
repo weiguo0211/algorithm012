5ce6501406ba222a8cbe4d871e4a469ce98a52aa
Wei Guo <guowei03@hexindai.com>
2020-07-25 14:38:45 +0800

    week1

--- Week_01/MergeSortedLinkedList.java
+++ Week_01/MergeSortedLinkedList.java
@@ -1,16 +1,32 @@
-public class MergeSortedLinkedList {
-    public List<Integer> inorderTraversal(TreeNode root) {
-        List<Integer> visited = new ArrayList<>();
-        Stack<TreeNode> stack = new Stack<TreeNode>();
-        while(root!= null || !stack.empty()){
-            while(root!=null){
-                stack.push(root);
-                root = root.left;
-            }
-            root = stack.pop();
-            visited.add(root.val);
-            root = root.right;
+
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if(l1 == null){
+            return l2;
         }
-        return visited;
+        if(l2 == null){
+            return l1;
+        }
+        ListNode head = new ListNode();
+        ListNode tail = head;
+        while(l1 != null && l2 != null){
+            if(l1.val <= l2.val){
+                tail.next = l1;
+                tail = l1;
+                l1 = l1.next;
+            } else {
+                tail.next = l2;
+                tail = l2;
+                l2 = l2.next;
+            }
+        }
+
+        if(l1 == null){
+            tail.next = l2;
+        } else {
+            tail.next = l1;
+        }
+        return head.next;
     }
+
 }
